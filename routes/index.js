1a3c87702d15ca712827978b73ef376bb0c60451
lea laplise <lea laplise>
2016-03-17 15:11:05 +0100

    home view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Index', active:'/' });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About', active:'/about'  });
+});
+
+/* GET Product page. */
+router.get('/produits', function(req, res, next) {
+  res.render('produits', { title: 'Produits', active:'/produits'  });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact', active:'/contact'  });
 });
 
 module.exports = router;
