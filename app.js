1609eed1c21ee30c1419200284d55f9aaa27f5ff
Benjamin G <benjamin.godineau@gmail.com>
2016-05-06 03:50:23 +0200

    auth test

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var flash = require('connect-flash');
 
-var routes = require('./routes/index');
-var product = require('./routes/product');
-var users = require('./routes/users');
-var amazon = require('./routes/amazon');
+
+var dbConfig = require('./config/database.js');
+var mongoose = require('mongoose');
+mongoose.connect(dbConfig.url);
+
+var passport = require('passport');
+var expressSession = require('express-session');
 
 var app = express();
 
@@ -16,13 +20,26 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(expressSession({secret:'MySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+var initPassport = require('./config/initPassport');
+initPassport(passport);
+
+var routes = require('./routes/index')(passport);
+var product = require('./routes/product');
+var users = require('./routes/users');
+var amazon = require('./routes/amazon');
 
 app.use('/', routes);
 app.use('/users', users);
@@ -32,7 +49,7 @@ app.use('/amazon', amazon);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   res.render('404', {
-    
+
   })
 });
 
